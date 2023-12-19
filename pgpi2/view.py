cd833c934d5722ca898a5211e2a5034d6c86967c
rhit-windsors <windsors@rose-hulman.edu>
2023-12-19 10:38:11 -0500

    update view.py

--- pgpi2/view.py
+++ pgpi2/view.py
@@ -4,24 +4,40 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver import ActionChains
 
-websites = ["https://www.cognytics.com/home?TokenID=35fdc7aa-483d-4aae-b196-76945b94ad80&DashboardName=Machine%20Timeline&ShowMenu=false&ShowControlbar=false&ShowFilters=false", "https://www.cognytics.com/home?TokenID=47a29d35-8abd-406d-ba67-8ce6bfd5cc76&DashboardName=Machine%20Timeline&ShowMenu=false&ShowControlbar=false&ShowFilters=false"]
-refreshTime = (60 * 15)
+websites = ["https://www.cognytics.com/home?TokenID=47a29d35-8abd-406d-ba67-8ce6bfd5cc76&DashboardName=Machine%20Timeline&ShowMenu=false&ShowControlbar=false&ShowFilters=false", "https://www.cognytics.com/home?TokenID=052cd1db-78cd-47f8-8ed9-53ff8932fdd4&DashboardName=Machine%20Timeline&ShowMenu=false&ShowControlbar=false&ShowFilters=false"]
+refreshTime = (60 * 10)
+
+def writeToLog(text):
+    log = open("log.log", "a")
+    log.write(text + "\n")
+    log.flush()
+    os.fsync(log.fileno())
 
 def clickButton(driver):
     # try to click button
     a = ActionChains(driver)
     try:
         m = driver.find_element("xpath",
-                                "/html/body/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/blockquote[2]/div/div/div[5]/div[1]/div[2]")
+                                '/html/body/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/blockquote[2]/div/div/div[5]/div[1]')
         a.move_to_element(m).perform()
         #driver.find_element("id", "button-1021-btnIconEl").click()
         driver.find_element("id", "button-1022-btnIconEl").click()
-        a.move_by_offset(0, 0).perform()
+        writeToLog("Clicked button\n")
+        while True:
+            try:
+                a.move_by_offset(0, 50).perform()
+            except:
+                break
     except:
         pause.seconds(1)
         clickButton(driver)
 
 def run():
+    # clear the log on program start
+    log = open("log.log", "w")
+    log.write("")
+    log.close()
+    writeToLog("Starting program\n")
     currentSite = 0
     service = webdriver.FirefoxService("/home/admin/.cargo/bin/geckodriver")
     driver = webdriver.Firefox(service=service)
@@ -33,7 +49,7 @@ def run():
         pause.seconds(refreshTime)
         currentSite = (currentSite + 1) % len(websites)
         driver.get(websites[currentSite])
-        driver.refresh()
+        writeToLog("Switched to site " + str(currentSite) + "\n")
 
 
 if __name__ == '__main__':
